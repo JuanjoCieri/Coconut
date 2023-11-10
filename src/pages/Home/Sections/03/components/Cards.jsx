5f99cfd9adba6de903d30482f9b16b676ece6098
JuanjoCieri <juin@hotmail.com.ar>
2023-11-10 14:08:40 -0300

    change

--- src/pages/Home/Sections/03/components/Cards.jsx
+++ src/pages/Home/Sections/03/components/Cards.jsx
@@ -3,10 +3,10 @@ import Card from "./Card";
 export default function Cards() {
   return (
     <div className="flex flex-wrap gap-10">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card title="+10y" desc="We have surfed the digitalization process of several industries."/>
+      <Card title="+300" desc="Innovation advisory services for start-ups and new big-tech products."/>
+      <Card title="+200" desc="We have successfully completed a total of 200+ projects"/>
+      <Card title="+150" desc="We have successfully give colour to start-ups and company re-branding."/>
     </div>
   );
 }
